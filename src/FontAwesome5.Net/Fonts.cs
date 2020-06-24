acaa91dee35162fe4da1ff80bdbd8bf08c0006ea
Nicolas Pierre <nicolas.pierre@creonis.be>
2020-06-24 14:25:29 +0200

    update

---
+++ src/FontAwesome5.Net/Fonts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FontAwesome5
+{
+    /// <summary>
+    /// Provides FontFamilies and Typefaces of FontAwesome5.
+    /// </summary>
+    public static class Fonts
+    {
+        /// <summary>
+        /// FontAwesome5 Regular FontFamily
+        /// </summary>
+        public static readonly FontFamily RegularFontFamily = new FontFamily(new Uri("pack://application:,,,/FontAwesome5.Net;component/"), "./Fonts/#Font Awesome 5 Pro Regular");
+        /// <summary>
+        /// FontAwesome5 Solid FontFamily
+        /// </summary>
+        public static readonly FontFamily SolidFontFamily = new FontFamily(new Uri("pack://application:,,,/FontAwesome5.Net;component/"), "./Fonts/#Font Awesome 5 Pro Solid");
+        /// <summary>
+        /// FontAwesome5 Brands FontFamily
+        /// </summary>
+        public static readonly FontFamily BrandsFontFamily = new FontFamily(new Uri("pack://application:,,,/FontAwesome5.Net;component/"), "./Fonts/#Font Awesome 5 Brands Regular");
+
+        /// <summary>
+        /// FontAwesome5 Light FontFamily
+        /// </summary>
+        public static readonly FontFamily LightFontFamily = new FontFamily(new Uri("pack://application:,,,/FontAwesome5.Net;component/"), "./Fonts/#Font Awesome 5 Pro Light");
+
+        /// <summary>
+        /// FontAwesome5 DuoTone FontFamily
+        /// </summary>
+        public static readonly FontFamily DuoToneFontFamily = new FontFamily(new Uri("pack://application:,,,/FontAwesome5.Net;component/"), "./Fonts/#Font Awesome 5 DuoTone Solid");
+
+
+        /// <summary>
+        /// FontAwesome5 Regular Typeface
+        /// </summary>
+        public static readonly Typeface RegularTypeface = new Typeface(RegularFontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+        /// <summary>
+        /// FontAwesome5 Solid Typeface
+        /// </summary>
+        public static readonly Typeface SolidTypeface = new Typeface(SolidFontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+        /// <summary>
+        /// FontAwesome5 Brands Typeface
+        /// </summary>
+        public static readonly Typeface BrandsTypeface = new Typeface(BrandsFontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+
+        /// <summary>
+        /// FontAwesome5 Light Typeface
+        /// </summary>
+        public static readonly Typeface LightTypeface = new Typeface(LightFontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+
+        /// <summary>
+        /// FontAwesome5 DuoTone Typeface
+        /// </summary>
+        public static readonly Typeface DuoToneTypeface = new Typeface(DuoToneFontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+    }
+}
